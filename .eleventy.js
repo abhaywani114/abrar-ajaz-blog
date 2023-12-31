db08dbe1f580340088610c06b86617243c1396e6
Abrar Osmanli <abhaywani114@gmail.com>
2023-12-31 15:25:36 +0530

    sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -117,6 +117,7 @@ module.exports = function (eleventyConfig) {
 
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
+  eleventyConfig.addPassthroughCopy("./src/static/sitemap.xml");
 
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.png");
